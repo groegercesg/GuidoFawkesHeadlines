67f75af66797ed2ee585c574ea56e78730c62781
groegercesg <groegercesg@users.noreply.github.com>
2022-06-12 13:09:21 +0100

    Code roadmap for next steps

---
+++ get_all_headlines.py
@@ -0,0 +1,8 @@
+# if .pkl doesn't exist, new run, create one
+    # loop from 1 through to when title is wrong, adding to dataframe
+    # save as pkl
+# else
+    # load pkl, get bottom record
+    # load 1, if headline == bottom record stop, print up to date
+    # otherwise, add to dataframe, continue finding until headline == bottom record
+    # save as pkl
