2a03e8a29dc47f7cbfe822b44eac7b9025aed774
groegercesg <groegercesg@users.noreply.github.com>
2022-06-13 13:26:18 +0100

    Test of code working, gather time

--- get_all_headlines.py
+++ get_all_headlines.py
@@ -5,45 +5,43 @@ from datetime import datetime
 import requests
 
 def getLinksForArticles():
-    print("We are starting to gather Article links.")
+    print("We are starting to gather Article headlines and links.")
     # Website for episodes     
     TAG_URL = "https://order-order.com/page/"
-    valid_url = False
+    invalid_url = False
     current_page = 1
     num_added=0
     page_links= []
     
-    #while valid_url is not True:
-    page = requests.get(TAG_URL+str(current_page)+"/")
-    soup = BeautifulSoup(page.content, "html.parser")
-    if soup.title.string != "Page not found – Guido Fawkes":
-        links = soup.find_all("a", {"class": "link--title"})
-        for i in range(0, len(links)):
-            if links[i]['href'] not in [a[0] for a in page_links]:
-                page_links.append([links[i]['href'], "", None])
+    while invalid_url is not True:
+        page = requests.get(TAG_URL+str(current_page)+"/")
+        soup = BeautifulSoup(page.content, "html.parser")
         
-        for i in range(0, len(links)):    
-            text_clean = links[i].text.lstrip().rstrip().replace('\n', ' ')
-            if text_clean != None and text_clean != "\n" and text_clean != "" and text_clean.split()[0] != "mdi-timer":
-                page_links[num_added][1] = text_clean
-            if len(text_clean.split()) > 1:
-                if text_clean.split()[0] == "mdi-timer":
-                    date = datetime.strptime(' '.join(text_clean.split()[1:6]), '%d %B %Y @ %H:%M')
-                    page_links[num_added][2] = date
-                    num_added += 1
-            
+        if current_page > 30:
+            invalid_url = True
         
-        # print("In page: " + str(current_page) + ", we found: " + str(len(page_links)-original_size) + " articles.")
-        current_page += 1
-    else:
-        valid_url = True
+        if soup.title.string != "Page not found – Guido Fawkes":
+            links = soup.find_all("a", {"class": "link--title"})
+            for i in range(0, len(links)):
+                if links[i]['href'] not in [a[0] for a in page_links]:
+                    page_links.append([links[i]['href'], "", None])
             
-    print("Overall, we found " + str(len(page_links)) + " articles")
+            for i in range(0, len(links)):    
+                text_clean = links[i].text.lstrip().rstrip().replace('\n', ' ')
+                if text_clean != None and text_clean != "\n" and text_clean != "" and text_clean.split()[0] != "mdi-timer":
+                    page_links[num_added][1] = text_clean
+                if len(text_clean.split()) > 1:
+                    if text_clean.split()[0] == "mdi-timer":
+                        date = datetime.strptime(' '.join(text_clean.split()[1:6]), '%d %B %Y @ %H:%M')
+                        page_links[num_added][2] = date
+                        num_added += 1
+                
+            current_page += 1
+        else:
+            invalid_url = True
+            
+    print("Overall, we found " + str(len(page_links)) + " headlines")
     return page_links
-
-out = getLinksForArticles()
-for i in range(0, len(out)):
-    print(out[i])
 
 # if .pkl doesn't exist, new run, create one
     # loop from 1 through to when title is wrong, adding to dataframe
@@ -59,4 +57,6 @@ if my_file.is_file():
     print("exists")
 else:
     # file does not exist
-    print("not exists")
+    print("File does not exist, generating a new one!")
+    df = pd.DataFrame(getLinksForArticles(), columns=['Link', 'Headline', 'Post Time'])
+    df.to_pickle('headlines.pkl')
